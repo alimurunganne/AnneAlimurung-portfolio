de4eb6c6c34d772ecdf109aa0da06ffadb949ad7
alimurunganne <alimurungannenichole20@gmail.com>
2026-02-21 23:02:47 +0800

    give descriptions in about page

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -14,19 +14,19 @@ export class Projects {
   projects = [
     {
       title: 'Garage Cafe',
-      description: 'Garage Café is a website that showcases a neighborhood café offering quality drinks, light meals, and a cozy atmosphere for the community.',
+      description: 'Garage Café is a website that highlights a local café, featuring quality drinks, light meals, and a welcoming space for the community to relax and gather.',
       image: 'assets/Frame-1.png',
       link: 'https://visitgaragecafe.com/'
     },
     {
       title: 'Barangay Information System',
-      description: 'The Barangay Information System is a digital platform that provides easy access to barangay services and community information.',
+      description: 'The Barangay Information System is a web-based platform that streamlines barangay services by allowing residents to register, request documents, and access community updates online, while administrators efficiently manage records and information.',
       image: 'assets/Frame-2.png',
       link: 'https://github.com/cassandraarcilla/6WCSERVER-Final-Project.git'
     },
     {
       title: 'Memoir Of Art',
-      description: 'Memoir of Art is a website made using WordPress.com, and its purpose is to explore and share insights about art and creativity.',
+      description: 'Memoir of Art is a WordPress website designed to explore and share insights, ideas, and inspirations about art and creativity.',
       image: 'assets/Frame-3.png',
       link: 'https://memoirofart.wordpress.com/'
     }
